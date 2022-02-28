957697d79de9cae8532116dd8e8cac159d80f17d
Kukuh Nugroho <kukuh.nugroho@ti.ukdw.ac.id>
2022-02-28 17:02:29 +0700

    Fix Verifikasi Transaksi Admin

--- resources/views/admin/verifikasi_transaksi.blade.php
+++ resources/views/admin/verifikasi_transaksi.blade.php
@@ -6,53 +6,66 @@
 
     <div class="card">
         <div class="card-body">
-            @if (count($dataTransaksi) == 0)
+            @if (count($dataTransaksi['data']) == 0)
                 <div class="container-fluid center-item bg-white ellipsis">
                     <span class="fw-bold">Tidak Ada Permintaan Verifikasi Transaksi</span>
                 </div>
             @else
-                @foreach($dataTransaksi as $dt)
-                <div class="accordion accordion-flush" id="accordionFlushExample">
-                    <div class="accordion-item items-shadow">
-                        <h2 class="accordion-header" id="flush-heading{{$loop->index}}">
-                            <button class="accordion-button collapsed fw-bold text-dark shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{$loop->index}}" aria-expanded="false" aria-controls="flush-collapse{{$loop->index}}">
-                                {{$dt['_id']}}
-                            </button>
-                        </h2>
-                        <div id="flush-collapse{{$loop->index}}" class="accordion-collapse collapse" aria-labelledby="flush-heading{{$loop->index}}" data-bs-parent="#accordionFlushExample">
-                            <div class="table-responsive">
-                                <table class="table table-borderless table-hover " id="tabel-kategori" cellspacing="0" width="100%">
-                                    <tbody>
-                                        <tr>
-                                            <th scope="col" width="30%">Wilayah</th>
-                                            <td scope="col" width="60%">: {{$dt['wilayah_lapak']['nama_wilayah']}}</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="col" width="30%">Alamat Lapak</th>
-                                            <td scope="col" width="60%">: {{$dt['alamat_lapak']['detail_alamat']}}, Kel. {{$dt['alamat_lapak']['kelurahan']}}, Kec. {{$dt['alamat_lapak']['kecamatan']}}</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="col" width="30%">Deskripsi Lapak</th>
-                                            <td scope="col" width="60%">: {{$dt['deskripsi_lapak']}}</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="col" width="30%">No Whatsapp</th>
-                                            <td scope="col" width="60%">: {{$dt['no_telepon_lapak']}}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <div class="d-grid gap-2 p-2">
-                                    <a href="" class="btn btn-primary" id="update-status-button" type="button">VERIFY</a>
+                @php
+                    $count = 0;
+                @endphp
+                @foreach($dataTransaksi['data'] as $dt)
+                    @if ($dt['status'] == "PROSES VERIFIKASI")
+                        @php
+                            $count++;
+                        @endphp
+                        <div class="accordion accordion-flush" id="accordionFlushExample">
+                            <div class="accordion-item items-shadow">
+                                <h2 class="accordion-header" id="flush-heading{{$loop->index}}">
+                                    <button class="accordion-button collapsed fw-bold text-dark shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{$loop->index}}" aria-expanded="false" aria-controls="flush-collapse{{$loop->index}}">
+                                        Transaksi nomor {{ $dt['_id'] }}
+                                    </button>
+                                </h2>
+                                <div id="flush-collapse{{ $loop->index }}" class="accordion-collapse collapse" aria-labelledby="flush-heading{{$loop->index}}" data-bs-parent="#accordionFlushExample">
+                                    <div class="table-responsive">
+                                        <table class="table table-borderless table-hover " id="tabel-kategori" cellspacing="0" width="100%">
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="col" width="30%">Pembeli</th>
+                                                    <td scope="col" width="60%">: {{ $dt['user']['nama'] }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="col" width="30%">Nama Lapak</th>
+                                                    <td scope="col" width="60%">: {{ $dt['nama_lapak'] }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="col" width="30%">Status Transaksi</th>
+                                                    <td scope="col" width="60%">: {{ $dt['status'] }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="col" width="30%">Updated At</th>
+                                                    <td scope="col" width="60%">: {{ $dt['updated_date'] }}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                        <div class="d-grid gap-2 p-2">
+                                            <a href="" class="btn btn-primary" id="update-status-button" type="button">VERIFY</a>
+                                        </div>
+                                        <form id="status-update-form" action="{{  route('verifikasi-lapak.updateStatus')  }}" method="POST" class="d-none">
+                                            @csrf
+                                            <input type="text" value="{{$dt['_id']}}" name="idLapak" hidden>
+                                        </form>
+                                    </div>
                                 </div>
-                                <form id="status-update-form" action="{{  route('verifikasi-lapak.updateStatus')  }}" method="POST" class="d-none">
-                                    @csrf
-                                    <input type="text" value="{{$dt['_id']}}" name="idLapak" hidden>
-                                </form>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    @endif
                 @endforeach
+                @if ($count == 0)
+                    <div class="container-fluid center-item bg-white ellipsis">
+                        <span class="fw-bold">Tidak Ada Permintaan Verifikasi Transaksi</span>
+                    </div>
+                @endif
             @endif
         </div>
     </div>

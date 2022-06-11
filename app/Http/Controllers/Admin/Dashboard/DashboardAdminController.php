2a36c85d5d4b91bc3318e5eced3fc31f340cf620
Alvrialdo Sanjaya Dese <alvrialdo.sanjaya@ti.ukdw.ac.id>
2022-06-11 14:47:20 +0700

    update fitur dashboard admin

--- app/Http/Controllers/Admin/Dashboard/DashboardAdminController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardAdminController.php
@@ -16,16 +16,18 @@ class DashboardAdminController extends Controller
      */
     public function index()
     {
-        $dataLapak = Http::withToken(session('_jwtToken'))->get('https://dev-ecommerce-api.paroki-gmaklaten.web.id/api/lapak/get')->json();
-        $tmp_lapak = array();
+        // $dataLapak = Http::withToken(session('_jwtToken'))->get('https://dev-ecommerce-api.paroki-gmaklaten.web.id/api/lapak/get')->json();
+        // $tmp_lapak = array();
 
-        foreach ($dataLapak['data'] as $lapak) {
-            $responseLapak = Http::withToken(session('_jwtToken'))->get("https://dev-ecommerce-api.paroki-gmaklaten.web.id/api/lapak/detail/" . $lapak['_id'] . "/get")->collect();
+        // foreach ($dataLapak['data'] as $lapak) {
+        //     $responseLapak = Http::withToken(session('_jwtToken'))->get("https://dev-ecommerce-api.paroki-gmaklaten.web.id/api/lapak/detail/" . $lapak['_id'] . "/get")->collect();
 
-            array_push($tmp_lapak, $responseLapak['data']);
-        }
+        //     array_push($tmp_lapak, $responseLapak['data']);
+        // }
 
-        return view('admin/dashboard_admin', ['arrLapak' => $tmp_lapak]);
+        // return view('admin/dashboard_admin', ['arrLapak' => $tmp_lapak]);
+
+        return view('admin/dashboard_admin2', ['jwt_token' => session('_jwtToken')]);
     }
 
     /**
